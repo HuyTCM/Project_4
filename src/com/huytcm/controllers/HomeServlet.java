e882ffe9e609936ed2b148a8beaf96d43c434438
HuyTCM <huytcm.1708@gmail.com>
2019-08-22 22:10:34 +0700

    add handle home servlet

--- src/com/huytcm/controllers/HomeServlet.java
+++ src/com/huytcm/controllers/HomeServlet.java
@@ -1,5 +1,8 @@
 package com.huytcm.controllers;
 
+import com.huytcm.dao.PostDao;
+import com.huytcm.entities.Post;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,13 +10,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @WebServlet(name = "HomeServlet")
 public class HomeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String pageText = request.getParameter("txtPage");
+        int page = Integer.parseInt(pageText);
+        PostDao postDao = new PostDao();
+        List<Post> posts = postDao.getPostAtPage(page);
+        request.setAttribute("POSTS", posts);
+        response.sendRedirect("index.jsp");
     }
 }
